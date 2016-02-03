e17eb24835f8056cd638eff9b46f50909070757a
Harris, Michael <micharris@gannett.com>
2016-02-03 14:25:41 -0500

    updated read / write to cvs

--- aggregate-csv-files.py
+++ aggregate-csv-files.py
@@ -2,34 +2,35 @@ import os, sys
 import csv
 
 directories = []
+csvRows = []
 projectPath = '/repository/download/'+sys.argv[1] #%system.teamcity.projectName%
 csvfile = sys.argv[2] #responseTime.csv
 
 #create a list of paths to previous artifacts
 for dirname in os.walk(projectPath).next()[1]:
     directories.append(dirname)
+    print dirname
 
 #order the list from newest to oldest
 directories.reverse() 
 
-print directories
 i = 0
 
-#create aggregated CSV file with CSV headers
-with open('responseTimeAggregated.csv', 'wb') as outcsv:
-    writer = csv.writer(outcsv)
-    writer.writerow(["Date", "endpoint", "responseTime"])
-
+#read all previous csv files and add rows to list
 for dirPath in directories:
     while i < 10:
         i = i + 1
-        with open('responseTime.csv', 'r') as fin, open('/opt/tcagents/tc_cs_agent/temp/agentTmp/responseTimeAggregated.csv', 'w') as fout:
+        with open(projectPath+dirPath+csvfile, 'r') as fin:
             reader = csv.reader(fin)
-            writer = csv.writer(fout)
             for row in reader:
             	try:
-            		new_row = row
-            		writer.writerow(new_row+[rows[i]])
-            		i+=1
+                    csvRows.append(row)
             	except IndexError:
             		pass
+
+#create aggregated CSV file
+with open('/opt/tcagents/linuxtcagent/temp/agentTmp/responseTimeAggregated.csv', 'wb') as outcsv:
+    writer = csv.writer(outcsv)
+    writer.writerow(["Date", "endpoint", "responseTime"])
+    for row in csvRows:
+        writer.writerow(row)
