933ae89b21c3d82d9ac5567181ec336994556f9a
Harris, Michael <micharris@gannett.com>
2016-02-03 14:49:54 -0500

    o

--- oembed-load-test.py
+++ oembed-load-test.py
@@ -4,7 +4,7 @@ class MyTaskSet(TaskSet):
 
     @task(1)
     def task1(self):
-        self.client.get("embed-web/?url=http://www.usatoday.com/story/weather/2016/01/20/winter-storm-snow-mid-atlantic-northeast/79048448")
+        self.client.get("oembed-web/?url=http://www.usatoday.com/story/weather/2016/01/20/winter-storm-snow-mid-atlantic-northeast/79048448/")
 
 class MyLocust(HttpLocust):
     task_set = MyTaskSet
