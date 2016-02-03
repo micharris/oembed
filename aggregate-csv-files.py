9e0297c3c9f332dbe0e4927d80400c2eaf8779d9
Harris, Michael <micharris@gannett.com>
2016-02-03 15:46:37 -0500

    path update

--- aggregate-csv-files.py
+++ aggregate-csv-files.py
@@ -6,37 +6,37 @@ csvRows = []
 projectID = sys.argv[1] #%teamcity.project.id%
 buildConfName = sys.argv[2] #%system.teamcity.buildConfName%
 targetFile = sys.argv[3]
-projectPath = '/opt/teamcity/.BuildServer/system/artifacts/'+projectID+'/'+buildConfName #/opt/teamcity/.BuildServer/system/artifacts/LoadTest/StartLoadTest/
+projectPath = 'opt/teamcity/.BuildServer/system/artifacts/'+projectID+'/'+buildConfName #/opt/teamcity/.BuildServer/system/artifacts/LoadTest/StartLoadTest/
 
 print projectPath
 
 #create a list of paths to previous artifacts
-# for dirname in os.walk(projectPath).next()[1]:
-#     buildCount.append(dirname)
+for dirname in os.walk(projectPath).next()[1]:
+    buildCount.append(dirname)
 
-# #order the list from newest to oldest
-# buildCount.reverse() 
+#order the list from newest to oldest
+buildCount.reverse() 
 
-# i = 0
+i = 0
 
-# #read all previous csv files and add rows to list
-# for buildNumber in buildCount:
-#     while i < 10:
-#         i = i + 1
-#         with open(projectPath+'/'+buildNumber+'/.teamcity/logs/'+targetFile, 'r') as fin:
-#             reader = csv.reader(fin)
-#             for row in reader:
-#                 next(reader, None)  # skip the headers
-#             	try:
-#                     csvRows.append(row)
-#             	except IndexError:
-#             		pass
+#read all previous csv files and add rows to list
+for buildNumber in buildCount:
+    while i < 10:
+        i = i + 1
+        with open(projectPath+'/'+buildNumber+'/.teamcity/logs/'+targetFile, 'r') as fin:
+            reader = csv.reader(fin)
+            for row in reader:
+                next(reader, None)  # skip the headers
+            	try:
+                    csvRows.append(row)
+            	except IndexError:
+            		pass
 
-# #create aggregated CSV file
-# with open('/opt/tcagents/linuxtcagent/temp/agentTmp/responseTimeAggregated.csv', 'wb') as outcsv:
-#     writer = csv.writer(outcsv)
-#     writer.writerow(["Date", "endpoint", "responseTime"])
-#     for row in csvRows:
-#         writer.writerow(row)
+#create aggregated CSV file
+with open('/opt/tcagents/linuxtcagent/temp/agentTmp/responseTimeAggregated.csv', 'wb') as outcsv:
+    writer = csv.writer(outcsv)
+    writer.writerow(["Date", "endpoint", "responseTime"])
+    for row in csvRows:
+        writer.writerow(row)
 
 #/opt/teamcity/.BuildServer/system/artifacts/LoadTest/StartLoadTest/136/.teamcity/logs/buildLog.msg5
