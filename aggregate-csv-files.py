4113c1da64e7b22e1ba0993e1dbb9db7786f8d59
Harris, Michael <micharris@gannett.com>
2016-02-03 15:15:05 -0500

    skip first row

--- aggregate-csv-files.py
+++ aggregate-csv-files.py
@@ -1,28 +1,30 @@
 import os, sys
 import csv
 
-directories = []
+buildCount = []
 csvRows = []
-projectPath = '/repository/download/'+sys.argv[1] #%system.teamcity.projectName%
-csvfile = sys.argv[2] #responseTime.csv
+projectID = sys.argv[1] #%teamcity.project.id%
+buildConfName = sys.argv[2] #%system.teamcity.buildConfName%
+targetFile = sys.argv[3]
+projectPath = '/opt/teamcity/.BuildServer/system/artifacts/'+projectID+'/'+buildConfName #/opt/teamcity/.BuildServer/system/artifacts/LoadTest/StartLoadTest/
 
 #create a list of paths to previous artifacts
 for dirname in os.walk(projectPath).next()[1]:
-    directories.append(dirname)
-    print dirname
+    buildCount.append(dirname)
 
 #order the list from newest to oldest
-directories.reverse() 
+buildCount.reverse() 
 
 i = 0
 
 #read all previous csv files and add rows to list
-for dirPath in directories:
+for buildNumber in buildCount:
     while i < 10:
         i = i + 1
-        with open(projectPath+dirPath+csvfile, 'r') as fin:
+        with open(projectPath+'/'+buildNumber+'/.teamcity/logs/'+targetFile, 'r') as fin:
             reader = csv.reader(fin)
             for row in reader:
+                next(reader, None)  # skip the headers
             	try:
                     csvRows.append(row)
             	except IndexError:
@@ -34,3 +36,5 @@ with open('/opt/tcagents/linuxtcagent/temp/agentTmp/responseTimeAggregated.csv',
     writer.writerow(["Date", "endpoint", "responseTime"])
     for row in csvRows:
         writer.writerow(row)
+
+#/opt/teamcity/.BuildServer/system/artifacts/LoadTest/StartLoadTest/136/.teamcity/logs/buildLog.msg5
